0fa03b5a8ef4477361af07e92e2d0d78b0648b82
Jorge Fernandez <jorge.fernandez@kapa7.com>
2021-04-27 22:01:32 -0600

    added update script and added caps to docker containers

---
+++ update.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+
+docker pull dcmlinux/mysql
+docker pull dcmlinux/nginx
+docker pull dcmlinux/dcmlinux-manager
+docker pull dcmlinux/slapd-dcm4chee
+docker pull dcmlinux/postgres-dcm4chee
+docker pull dcmlinux/dcm4chee-arc-psql-dcmlinux
